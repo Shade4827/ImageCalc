edb2256230de44a246af4e6cb3372c038267cbce
Shade4827 <seahorses527@gmail.com>
2020-09-27 10:44:11 +0900

    [追加]ファイルの選択ができるようになったが表示ができない

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import sys
-sys.path.append('D:/anaconda3/envs/ImageCalc/Lib/site-packages')
 import cv2
 import os
 import tkinter as tk
 import tkinter.ttk as ttk
 import tkinter.filedialog
+import shutil
 from PIL import Image,ImageTk 
 from functools import partial
 from tkinter import messagebox,simpledialog
@@ -16,15 +16,15 @@ class Application(tk.Frame):
         self.fileName=["",""]
         self.fileLabel=[tk.Label(root),tk.Label(root)]
         self.fileLabelPlace=[[240,640],[900,640]]
+        self.image=[cv2.imread("ex1.png"),cv2.imread("ex2.png")]
 
         self.CreateWigets()
 
     def CreateWigets(self):
         #画像表示用キャンバス
-        self.canvas1=tk.Canvas(root,width=500,height=500)
-        self.canvas1.place(x=-640,y=0)
-        self.canvas2=tk.Canvas(root,width=500,height=500)
-        self.canvas2.place(x=640,y=0)
+        self.canvas=[tk.Canvas(root,width=500,height=500),tk.Canvas(root,width=500,height=500)]
+        self.canvas[0].place(x=0,y=0)
+        self.canvas[1].place(x=640,y=0)
 
         #ファイル選択ボタン
         image1Button=tk.Button(root,text="画像1を選択",width=15,font=("",15),command=partial(self.SelectImage,0))
@@ -47,10 +47,24 @@ class Application(tk.Frame):
             self.fileLabel[num].destroy()
             self.fileLabel[num]=tk.Label(root,text=fileName,font=("",15),justify="center")
             self.fileLabel[num].place(x=self.fileLabelPlace[num][0],y=self.fileLabelPlace[num][1])
+            self.ReadImage(num)
+            self.DisplayImage(num)
 
-    #マージ実行
+    #マージ実行/表示
     def MargeImage(self):
         a=0
+
+    #画像読み込み
+    def ReadImage(self,num):
+        shutil.copy(self.fileName[num],"./image"+str(num+1)+".png")
+        self.image[num]=cv2.imread("./image"+str(num+1)+".png")
+        self.image[num]=cv2.resize(self.image[num],dsize=(500,500))
+        self.image[num]=cv2.cvtColor(self.image[num],cv2.COLOR_BGR2RGB)
+
+    #画像表示
+    def DisplayImage(self,num):
+        img=ImageTk.PhotoImage(Image.fromarray(self.image[num]))
+        self.canvas[num].create_image(0,0,image=img,anchor=tk.NW)
 
 root = tk.Tk()
 myapp = Application(master=root)
